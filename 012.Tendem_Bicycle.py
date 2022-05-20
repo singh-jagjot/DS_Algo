f31e2a73bf34d9fa82f9f09404081b8ec8953acf
singh-jagjot <jagjot.no1@gmail.com>
2022-05-20 19:31:37 -0400

    sol 12

---
+++ 012.Tendem_Bicycle.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def tendemBicycle(red: List, blue: List, fastest: bool) -> int:
+    red.sort()
+    if fastest:
+        blue.sort(reverse=True)
+    else:
+        blue.sort()
+
+    tot = 0
+    for idx in range(len(red)):
+        tot += max(red[idx], blue[idx])
+    return tot
