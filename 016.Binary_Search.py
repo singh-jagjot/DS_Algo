b5c8f63027db3ce93c2700043c2c15739e94122d
singh-jagjot <jagjot.no1@gmail.com>
2022-05-21 11:56:00 -0400

    update

--- 016.Binary_Search.py
+++ 016.Binary_Search.py
@@ -1,6 +1,6 @@
 from typing import List
 
-
+# Recursive
 def binarySearch(arr: List, target) -> bool:
     return binarySearchHelper(arr, target, 0, len(arr) - 1)
 
@@ -18,5 +18,18 @@ def binarySearchHelper(arr: List, target: int, left: int, right: int) -> bool:
     else:
         return binarySearchHelper(arr, target, mid + 1, right)
 
+#Iterative
+def binarySearchItr(arr: List, target: int) -> bool:
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target == arr[mid]:
+            return True
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return False
 
-print(binarySearch([1, 3, 4, 6, 7, 9, 23], 233))
+print(binarySearchItr([1, 3, 4, 6, 7, 9, 23], 23))
