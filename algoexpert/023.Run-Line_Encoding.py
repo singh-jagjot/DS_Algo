8824d37bed068afbdc0cdd690a1aa2f9032411f6
singh-jagjot <jagjot.no1@gmail.com>
2022-05-23 13:44:26 -0400

    sol 23

---
+++ algoexpert/023.Run-Line_Encoding.py
@@ -0,0 +1,15 @@
+def runLineEncoding(s: str) -> str:
+    head = 1
+    rle = []
+    for i in range(1, len(s)):
+        if s[i] != s[i - 1] or head == 9:
+            rle.append(str(head))
+            rle.append(s[i-1])
+            head = 0
+        head += 1
+    rle.append(str(head))
+    rle.append(s[len(s) - 1])
+    return ''.join(rle)
+
+
+print(runLineEncoding('AAAAAAAAAAAAABBCCCCDD'))
