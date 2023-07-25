3bf0631f7657bd5b63e1fbbce3ceeae652d8d9b3
singh-jagjot <jagjot.no1@gmail.com>
2023-07-25 08:27:55 -0400

    +tranpose_matrix

---
+++ algoexpert/Easy/Transpose_Matrix.py
@@ -0,0 +1,13 @@
+def transposeMatrix(matrix):
+    # Write your code here.
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    mat = []
+    for i in range(cols):
+        row = []
+        for j in range(rows):
+            row.append(matrix[j][i])
+        mat.append(row)
+            
+    return mat
