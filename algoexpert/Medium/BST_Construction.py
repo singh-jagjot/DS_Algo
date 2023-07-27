f01247131e1688fd831bd00588d9202b4325016f
singh-jagjot <jagjot.no1@gmail.com>
2023-07-27 14:56:24 -0400

    Update

--- algoexpert/Medium/BST_Construction.py
+++ algoexpert/Medium/BST_Construction.py
@@ -1,13 +1,16 @@
-from typing import Any
-
-
+# Do not edit the class below except for
+# the insert, contains, and remove methods.
+# Feel free to add new properties and methods
+# to the class.
 class BST:
-    def __init__(self, value) -> None:
-        self.value: Any = value
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
 
     def insert(self, value):
+        # Write your code here.
+        # Do not edit the return statement of this method.
         current_node = self
         while True:
             if value < current_node.value:
@@ -26,6 +29,7 @@ class BST:
         return self
 
     def contains(self, value):
+        # Write your code here.
         current_node = self
         while current_node is not None:
             if value < current_node.value:
@@ -42,9 +46,10 @@ class BST:
             current_node = current_node.left
         return current_node.value
 
-    # Here we added 'parent' parameter to remove the currentNode if it doesn't have either of left or right child.
-
-    def remove(self, value, parent=None):
+    
+    def remove(self, value, parent = None):
+        # Write your code here.
+        # Do not edit the return statement of this method.
         current_node = self
         while current_node is not None:
             if value < current_node.value:
@@ -59,8 +64,8 @@ class BST:
                     current_node.right.remove(current_node.value, current_node)
                 # This case is for a root node where it doesn't have either of left or right child.
                 elif parent is None:
-                    # Here value of current_node.left and current_node.right is not 'None' because not
-                    # either child of root node can have further children.
+                    # Here value of current_node.left or current_node.right is not 'None' because
+                    # they can have further children.
                     if current_node.left is not None:
                         # Beware of the order here or else we can get null pointer exception
                         current_node.value = current_node.left.value
@@ -71,10 +76,14 @@ class BST:
                         current_node.left = current_node.right.left
                         current_node.right = current_node.right.right
                     else:
-                        current_node.value = None
+                        # Requirement of the Algoexpert solution
+                        pass
+                        # This will delete the BST
+                        # current_node.value = None
                 elif parent.left == current_node:
                     parent.left = current_node.left if current_node.right is None else current_node.right
-                elif parent.right is current_node:
+                elif parent.right == current_node:
                     parent.right = current_node.left if current_node.right is None else current_node.right
                 break
+
         return self
