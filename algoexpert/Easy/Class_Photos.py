fc26876cdcfa29e1b2ddcf9e4cb79feb7df3e0fd
singh-jagjot <jagjot.no1@gmail.com>
2023-07-28 05:11:21 -0400

    refactor

---
+++ algoexpert/Easy/Class_Photos.py
@@ -0,0 +1,15 @@
+def classPhotos(redShirtHeights, blueShirtHeights):
+    # Write your code here.
+    redShirtHeights.sort()
+    blueShirtHeights.sort()
+    backRow = 'r' if redShirtHeights[0] > blueShirtHeights[0] else 'b'
+
+    for i in range(len(redShirtHeights)):
+        if backRow == 'r':
+            if redShirtHeights[i] <= blueShirtHeights[i]:
+                return False
+        else:
+            if redShirtHeights[i] >= blueShirtHeights[i]:
+                return False
+            
+    return True
---
+++ algoexpert/Easy/Tandem_Bicycle.py
@@ -0,0 +1,12 @@
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+    # Write your code here.
+    redShirtSpeeds.sort()
+    if fastest:
+        blueShirtSpeeds.sort(reverse=True)
+    else:
+        blueShirtSpeeds.sort()
+
+    tot = 0
+    for idx in range(len(redShirtSpeeds)):
+        tot += max(redShirtSpeeds[idx], blueShirtSpeeds[idx])
+    return tot
