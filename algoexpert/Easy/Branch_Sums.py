f9891179f97ad7f8a5ec7ee5422a344fb6bc5770
singh-jagjot <jagjot.no1@gmail.com>
2023-07-28 16:26:38 -0400

    update

---
+++ algoexpert/Easy/Branch_Sums.py
@@ -0,0 +1,25 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+    # Write your code here.
+    lst = []
+    helper(root, 0, lst)
+    return lst
+
+def helper(node, sum, lst):
+    if node is None :
+        return
+    
+    sum += node.value
+    if node.left is None and node.right is None:
+        lst.append(sum)
+    else:
+        helper(node.left, sum, lst)
+        helper(node.right, sum, lst)
+    
