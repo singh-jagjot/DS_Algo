3306ee95ef833d988291bcefa3828d23879b063c
singh-jagjot <jagjot.no1@gmail.com>
2023-08-11 03:15:52 -0400

    +Missing_Numbers

--- algoexpert/Medium/Missing_Numbers.py
+++ algoexpert/Medium/Missing_Numbers.py
@@ -1,7 +1,20 @@
+# O(n) time | O(1) space
 def missingNumbers(nums):
     # Write your code here.
     total = sum(nums)
-    missing_sum = (len(nums) + 2) * (len(nums) + 3)//2 - total
-    return missing_sum
+    # Sum of N terms of an AP is used here
+    missingTotal = (len(nums) + 2) * (len(nums) + 3)//2 - total
+    pivot = missingTotal//2
+    firstHalfSum = secondHalfSum = 0
+    for num in nums:
+        if num <= pivot:
+            firstHalfSum += num
+        else:
+            secondHalfSum += num
+    # Sum of N terms of an AP is used here
+    firstMissingNumber = (pivot + 1)*pivot//2 - firstHalfSum
+    secondMisingNumber = missingTotal - firstMissingNumber
+    return [firstMissingNumber, secondMisingNumber]
 
-print(missingNumbers([1,2,3]))
+
+print(missingNumbers([1, 2, 3]))
