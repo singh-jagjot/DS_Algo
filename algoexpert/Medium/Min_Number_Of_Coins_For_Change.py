b1e61b902a26387e6d26df5ec12b7b7485d185ad
singh-jagjot <jagjot.no1@gmail.com>
2023-09-24 23:57:18 -0400

    +Min_Number_Of_Coins_For_Change

---
+++ algoexpert/Medium/Min_Number_Of_Coins_For_Change.py
@@ -0,0 +1,12 @@
+# O(n*d) time | O(n) space
+def minNumberOfCoinsForChange(n, denoms):
+    # Write your code here.
+    numOfCoins = [float('inf') for _ in range(n + 1)]
+    numOfCoins[0] = 0
+    for denom in denoms:
+        for amount in range(len(numOfCoins)):
+            if denom <= amount:
+                numOfCoins[amount] = min(
+                    numOfCoins[amount], 1 + numOfCoins[amount - denom])
+
+    return numOfCoins[n] if numOfCoins[n] != float('inf') else -1
