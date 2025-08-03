7ee1743b99b0c09a0bd8c02713aaa9536a7e9773
singh-jagjot <jagjot.no1@gmail.com>
2025-08-03 14:57:09 -0400

    785 improved

--- leetcode/java/0785. Is Graph Bipartite.java
+++ leetcode/java/0785. Is Graph Bipartite.java
@@ -1,33 +1,53 @@
+// Using BFS Coloring
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int[] visited = new int[graph.length];
-        int[] partition = new int[graph.length];
+        int[] color = new int[graph.length]; // 0 = uncolored, 1/2 = partitions
 
-        Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < graph.length; i++) {
-            if(visited[i]==0){
-                partition[i] = 1;
-                stack.push(i);
-            }
-            while (!stack.isEmpty()) {
-                int node = stack.peek();
-                if(visited[node] == 0) visited[node] = 1;
-                boolean neighboursVisited = true;
-                for(int neighbour: graph[node]){
-                    if(visited[neighbour] == 0) {
-                        stack.push(neighbour);
-                        neighboursVisited = false;
-                        if(partition[node] == 1) partition[neighbour] = 2;
-                        else partition[neighbour] = 1;
-                        break;
-                    }
-                    if(partition[node] == partition[neighbour]) return false;
+            if (color[i] != 0) continue;
 
+            Queue<Integer> queue = new ArrayDeque<>();
+            queue.offer(i);
+            color[i] = 1;
+
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+                for (int neighbor : graph[node]) {
+                    if (color[neighbor] == 0) {
+                        color[neighbor] = 3 - color[node]; // flip between 1 and 2
+                        queue.offer(neighbor);
+                    } else if (color[neighbor] == color[node]) {
+                        return false; // same color on both ends → not bipartite
+                    }
                 }
-                if(neighboursVisited) {
-                    visited[node] = 2;
-                    stack.pop();
-                    System.out.println();
+            }
+        }
+
+        return true;
+    }
+}
+
+// Using Iterative DFS Coloring
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int[] color = new int[graph.length]; // 0 = unvisited, 1 = red, -1 = blue
+
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] != 0) continue;
+
+            Deque<Integer> stack = new ArrayDeque<>();
+            stack.push(i);
+            color[i] = 1;
+
+            while (!stack.isEmpty()) {
+                int node = stack.pop();
+                for (int neighbor : graph[node]) {
+                    if (color[neighbor] == 0) {
+                        color[neighbor] = -color[node]; // assign opposite color
+                        stack.push(neighbor);
+                    } else if (color[neighbor] == color[node]) {
+                        return false; // same color conflict
+                    }
                 }
             }
         }
